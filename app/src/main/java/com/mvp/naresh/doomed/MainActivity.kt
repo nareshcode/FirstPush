7fd7a705fcacc457922bff1e65233829ac88a9c3
nareshcode <cherry.naresh52@gmail.com>
2018-02-16 01:26:27 +0530

    Added extra data

--- app/src/main/java/com/mvp/naresh/doomed/MainActivity.kt
+++ app/src/main/java/com/mvp/naresh/doomed/MainActivity.kt
@@ -9,5 +9,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //MSTICK
+        //MSTICK
+        //MSTICK//MSTICK
+        //MSTICK
+        //MSTICK
+        //MSTICK
+        //MSTICK
+
     }
 }
